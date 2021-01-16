1f13253860c22659b59b4798ed206600faebcbe8
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2021-01-16 00:14:27 -0500

    deploy

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,6 +128,7 @@ MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_ROOT = BASE_DIR / "media_root"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static').
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
