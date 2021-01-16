4e4bb67f60132527fe764ca36602ca5e3028bb0a
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2021-01-16 00:19:40 -0500

    deploy

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -128,7 +128,7 @@ MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_ROOT = BASE_DIR / "media_root"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static').
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
